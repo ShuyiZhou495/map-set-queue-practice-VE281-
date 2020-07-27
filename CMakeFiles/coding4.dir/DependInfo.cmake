80e46aeeb1b7721b79cadb7ab93d9402c65df7f1
ShuyiZhou495 <zhoushuyi495@gmail.com>
2020-07-27 23:43:49 +0200

    first commit

---
+++ CMakeFiles/coding4.dir/DependInfo.cmake
@@ -0,0 +1,23 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/zhoushuyi/Documents/VE281/HW/coding4/equity.cpp" "/Users/zhoushuyi/Documents/VE281/HW/coding4/cmake-build-debug/CMakeFiles/coding4.dir/equity.cpp.o"
+  "/Users/zhoushuyi/Documents/VE281/HW/coding4/main.cpp" "/Users/zhoushuyi/Documents/VE281/HW/coding4/cmake-build-debug/CMakeFiles/coding4.dir/main.cpp.o"
+  "/Users/zhoushuyi/Documents/VE281/HW/coding4/order.cpp" "/Users/zhoushuyi/Documents/VE281/HW/coding4/cmake-build-debug/CMakeFiles/coding4.dir/order.cpp.o"
+  "/Users/zhoushuyi/Documents/VE281/HW/coding4/simulation.cpp" "/Users/zhoushuyi/Documents/VE281/HW/coding4/cmake-build-debug/CMakeFiles/coding4.dir/simulation.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "AppleClang")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ CMakeFiles/coding4.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/coding4.dir/equity.cpp.o"
+  "CMakeFiles/coding4.dir/main.cpp.o"
+  "CMakeFiles/coding4.dir/order.cpp.o"
+  "CMakeFiles/coding4.dir/simulation.cpp.o"
+  "coding4"
+  "coding4.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/coding4.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
